0ed3103f4431ba9ce7d91215cb3b2225c6ac336a
cuki2 <mauricio@krebs.com.br>
2015-07-07 20:06:42 -0300

    read / write

--- src/serial.c
+++ src/serial.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv) {
 
 	fd = open_port(argv[1]);
 
-	set_port(9600, fd);
+	set_port(19200, fd);
 
 	make_request(1, 3, 0, 10, str);
 
@@ -37,16 +37,16 @@ int main(int argc, char **argv) {
 
 	usleep(5000);
 
-//	n = -1;
-//	while (n == -1) {
-//		n = read(fd, buffer, 1024);
-//		tentativas++;
-//	}
+	n = -1;
+	while (n == -1) {
+		n = read(fd, buffer, 1024);
+		tentativas++;
+	}
 
 	printf("lido %d bytes\n\r", n);
 	if (n > 0)
 		for (cont = 0; cont < n; ++cont)
-			printf("0x%X ", str[cont]);
+			printf("0x%X ", buffer[cont]);
 	printf("\n\rTetativas de leitura %d\n\r", tentativas);
 
 	close(fd);
